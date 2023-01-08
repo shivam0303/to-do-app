a7596fa7de5a200b63fcb4f1c40aece02d2cc479
Shivam Verma <77184110+shivam0303@users.noreply.github.com>
2023-01-08 16:28:42 +0530

    Update date.js

--- date.js
+++ date.js
@@ -15,17 +15,3 @@ function getDate(){
 }
 
 
-//OR
-// exports.getDate = function (){
-//     let today = new Date();
-//     let currentDay = today.getDay();
-    
-//     let options = {
-//         weekday : "long",
-//         day : "numeric",
-//         month : "long"
-//     };
-
-//     let day = today.toLocaleDateString("en-US",options);
-//     return day;
-// }
